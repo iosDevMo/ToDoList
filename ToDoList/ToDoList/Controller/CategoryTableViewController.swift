5e6cf2b8870035a9a041746b733f144db51c61a1
mohamdan <engineerm86@gmail.com>
2023-07-17 14:36:20 +0300

    add table view controller

---
+++ ToDoList/ToDoList/Controller/CategoryTableViewController.swift
@@ -0,0 +1,49 @@
+//
+//  CategoryTableViewController.swift
+//  ToDoList
+//
+//  Created by mohamdan on 17/07/2023.
+//
+
+import UIKit
+
+class CategoryTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    
+    @IBAction func addCategory(_ sender: UIBarButtonItem) {
+        
+        
+        
+    }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "categoryCell", for: indexPath)
+        
+        // Configure the cell...
+        
+        return cell
+    }
+    
+}
